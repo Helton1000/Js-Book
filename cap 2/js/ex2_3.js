43117cd24e731ee920b7dc062099e4f24a410a48
Helton Barbosa <helton1000xd8@gmail.com>
2023-04-10 21:55:23 -0300

    new task

---
+++ cap 2/js/ex2_3.js
@@ -0,0 +1,21 @@
+function mostrarPromo(){
+    // Reference
+    let inCarro = document.getElementById("inCarro");
+    let inPrice = document.getElementById("inPrice");
+    let outCarro = document.getElementById("outCarro");
+    let outEntrada = document.getElementById("outEntrada");
+    let outParcelas = document.getElementById("outParcelas");
+
+    // Content
+    let car = inCarro.value;
+    let price = Number(inPrice.value);
+
+    // Entrada
+    let entrada = price * 0.5;
+    let parcela = (price * 0.5) / 12;
+
+    // Alterar 
+    outCarro.innerHTML = "Promoção:" + car;
+    outEntrada.innerHTML = "Entrada R$:" + entrada;
+    outParcelas.innerHTML = "+ 12x de R$:" + parcela.toFixed(2);
+}
